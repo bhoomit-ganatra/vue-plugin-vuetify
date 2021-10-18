e86b2849496232e21bb9591979ab26da568f3de8
plugin <plugin@plugin.com>
2021-10-18 19:35:37 +0530

    plugin worked

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,36 @@
 <template>
-  <div id="app">
-    <div v-html="gretings"></div>
-    <form @submit.prevent="updateToast(msg)">
-      <input type="text" name="msg" id="msg" v-model="msg" />
-      <button> Submit </button>
-    </form>
-  </div>
+  <v-app>
+    <div class="text-center">
+      <v-btn dark color="red darken-2" @click="toggleSnackbar(true)">
+        Open Snackbar
+      </v-btn>
+
+      <v-snackbar v-model="toast.show">
+        {{ toast.message }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn color="red" text v-bind="attrs" @click="toggleSnackbar(false)">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-  inject: ["toast", "updateToast"],
-  data: () => ({
-    gretings: "gretings",
-    msg: ''
-  }),
-  watch: {
-    toast: {
-      deep: true,
-      immediate: true,
-      handler(newVal) {
-        console.log('new data for toast is ', newVal)
-      }
-    }
-  },
+  inject: ['toast','updateToast'],
 
-  beforeMount() {
-    this.gretings = this.$bold(this.gretings);
-    console.log("toast data is ", this.toast);
-    console.log("update toast data is ", this.updateToast);
-  },
+  data: () => ({
+    snackbar: false,
+    text: 'ok'
+  }),
+  methods: {
+    toggleSnackbar(isOpen) {
+      isOpen ? this.updateToast('this is a toast message') : this.updateToast()
+    }
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import plugin from "@/plugins/plugins.js";
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(plugin)
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
